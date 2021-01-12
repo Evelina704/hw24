41c40b1c0118c16dbc23ccdc57ed5f252b137f3b
Hp <anevelina0@gmail.com>
2021-01-12 23:32:02 +0300

    add project

---
+++ bread.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class bread implements cookable{
+    @Override
+    public void cook() {
+        System.out.println("bread is cooking");
+    }
+}
---
+++ circle.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class circle implements drawable{
+    @Override
+    public void draw(){
+        System.out.println("Circle is drawing");
+    }
+}
